7da50332006159623eb9b5b2c9cb929f9865b23e
ahlam201601 <elhadriahlam9@gmail.com>
2025-11-01 00:41:58 +0100

    Affichage le nom aleatoire

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 let tableau = []
-
 const ajouterNomBtn = document.getElementById("ajouterNomBtn")
 const MyListe = document.getElementById("MyListe")
+
+const popupMessageBtn = document.getElementById("popupMessageBtn")
+
 
 ajouterNomBtn.addEventListener("click", function AjouterNom(){
 
       const li = document.createElement("li")
       const text = document.getElementById("text").value
-      li.innerText = text;                                 
-      MyListe.appendChild(li);   
+      if (text === "") return;
+      tableau.push(text);
+      MyListe.appendChild(li).innerHTML = text
+      //console.log(tableau);   
        
 })
+
+
+
+
+
+
+
+
+popupMessageBtn.addEventListener("click", function ChoisirNom() {
+      const nomAleatoire = tableau[Math.floor(Math.random() * tableau.length)]
+      //console.log(nomAleatoire);
+})
+
 
 
 
